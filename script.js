bf2bd616514b1030979efce7566c52bb0eadd2b6
RaghavS4901 <89128983+RaghavS4901@users.noreply.github.com>
2021-12-31 20:24:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Artificial Intelligence?"],
+        strings: [""],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
